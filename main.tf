739cf3c878cd5ea9b62ccf2a09a778949ecf6782
Christian Stalder <ch.stalder@gmail.com>
2023-06-27 22:22:06 +0200

    V2

--- main.tf
+++ main.tf
@@ -17,23 +17,23 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = var.resource_group_location
-  
-tags                = {
-	"ENV"      = "PlayGround"
+
+  tags = {
+    "ENV"      = "PlayGround"
     "LOCATION" = "Schweiz"
-	}
+  }
 }
 
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = "myTFVnet"
-  address_space       = ["10.0.0.0/24"]
+  address_space       = ["10.0.0.0/23"]
   location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.rg.name
-  
-  tags                = {
-	"ENV"      = "PlayGround"
+
+  tags = {
+    "ENV"      = "PlayGround"
     "LOCATION" = "Schweiz"
-	}
-  
+  }
+
 }
